36b252fc6e24218b8e62881bb9a325688a6c30de
Andrew Auclair <mightymalakai33@gmail.com>
2023-10-30 19:12:11 -0400

    Tasks can be added, started and retrieved.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,24 @@
 import java.io.*;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.time.OffsetDateTime;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import org.json.JSONObject;
 
 public class Main {
+    private static void sendJSON(DataOutputStream output, JSONObject object) throws IOException {
+        String string = object.toString();
+
+        short size = (short) (2 + string.length());
+
+        output.write(ByteBuffer.allocate(2).putShort(size).array());
+        output.write(string.getBytes());
+
+        output.flush();
+    }
+
     public static void main(String[] args) throws IOException {
         System.out.println("Connect to micro-task C++ CLI");
 
@@ -18,15 +31,9 @@ public class Main {
         JSONObject versionRequest = new JSONObject();
         versionRequest.put("command", 1);
 
-        String string = versionRequest.toString();
+        sendJSON(output, versionRequest);
 
-        short size = (short) (2 + string.length());
-
-        output.write(ByteBuffer.allocate(2).putShort(size).array());
-        output.write(string.getBytes());
-
-        output.flush();
-
+        Thread listen = new Thread(() -> {
         try (DataInputStream in = new DataInputStream(socket.getInputStream())) {
             int packetLength;
             while ((packetLength = in.readShort()) != -1) {
@@ -60,8 +67,55 @@ public class Main {
                     case 3: // start task
                         break;
                     case 4: // get task response
+                        OffsetDateTime addTime = OffsetDateTime.parse(obj.getString("add-time"));
+                        System.out.printf("Task %d %s %s%n", obj.getInt("id"), addTime, obj.getString("name"));
+
+                        if (obj.has("start-time")) {
+                            OffsetDateTime startTime = OffsetDateTime.parse(obj.getString("start-time"));
+
+                            System.out.println("start time: " + startTime);
+                        }
                         break;
                 }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        });
+
+        listen.start();
+
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+
+            if (line.startsWith("add")) {
+                String name = line.substring(4);
+
+                JSONObject addTask = new JSONObject();
+                addTask.put("command", 2);
+                addTask.put("name", name);
+
+                sendJSON(output, addTask);
+            }
+            else if (line.startsWith("start")) {
+                int id = Integer.parseInt(line.substring(6));
+
+                JSONObject startTask = new JSONObject();
+                startTask.put("command", 3);
+                startTask.put("id", id);
+
+                sendJSON(output, startTask);
+            }
+            else if (line.startsWith("get")) {
+                int id = Integer.parseInt(line.substring(4));
+
+                JSONObject getTask = new JSONObject();
+                getTask.put("command", 4);
+                getTask.put("id", id);
+
+                sendJSON(output, getTask);
             }
         }
     }
